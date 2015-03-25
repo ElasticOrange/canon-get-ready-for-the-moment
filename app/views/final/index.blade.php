f8623b90a6c75863838ef8fc40a136f4d16d1619
mihaibarbu <xwave21@gmail.com>
2015-03-25 17:41:56 +0200

    Add final page

--- app/views/final/index.blade.php
+++ app/views/final/index.blade.php
@@ -1,20 +1,16 @@
 @extends('layout.master')
 
 @section('content')
-    <div class="container-picture">
+    <div class="final_page">
+        <img src="/img/final_page.png" />
+    </div>
+    <!-- <div class="container-picture">
         <div class="ugly-circle"></div>
         <img src="img/pagina_intro.png"/>
         <div class="text-intro text-intro-final">Multumim pentru participare!</div>
         <div class="text-intro-second"></div>
-    </div>
+    </div> -->
 
-    <!--Obiective-->
-    <input type="submit" class="submit-button" value="Submit"/>
-    <div class="obiectiv_unu obiective obiective-submit"><img src="img/Obiective/obiectiv2.png"/></div>
-    <div class="obiectiv_doi obiective obiective-submit"><img src="img/Obiective/obiectiv1.png"/></div>
-    <div class="obiectiv_trei obiective obiective-submit"><img src="img/Obiective/obiectiv4.png"/></div>
-    <div class="obiectiv_patru obiective obiective-submit"><img src="img/Obiective/obiectiv3.png"/></div>
-    <!--Obiective-->
 @stop
 
 @section('js')
