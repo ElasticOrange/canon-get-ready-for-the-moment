aae1a7b66ba608a08733e1af61f1db33c10e6b8c
mihaibarbu <xwave21@gmail.com>
2015-03-27 16:39:00 +0200

    Add diacritice to first page

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -5,9 +5,9 @@
         <img src="img/pagina_intro.png"/>
         <div class="ugly-circle"></div>
         <div class="text-intro"></div>
-        <div class="text-intro-second">Momente speciale te asteapta la orice pas! Pregateste-te sa le surprinzi 
-        si profita de discountul de 300 de Ron la achizitia unui obiectiv suplimentar. Parcurge aplicatia si 
-        descopera obiectivul preferat!</div>
+        <div class="text-intro-second">Momente speciale te aşteaptă la orice pas! Pregăteşte-te 
+        să le surprinzi şi profită de discountul de 300 de Ron la achiziţia unui obiectiv suplimentar. 
+        Parcurge aplicaţia şi descoperă obiectivul preferat!</div>
         <button type="button" class="button-descopera">Intră în aplicaţie aici!</button>
     </div>
 
